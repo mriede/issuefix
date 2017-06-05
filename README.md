ddce8f00bd46bce88a257d6c14c2468c4d6227fc
MeksPwnschlager <doctoranyk@gmail.com>
2017-06-05 23:09:48 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # issuefix
+
+Display a random issue and fix it (if you want to).
+
+```bash
+npm install;
+npm start;
+```
---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./issuefix.db');
+const app = express();
+
+const apiUrl = "https://api.github.com"
+
+function requestWithUserAgent(url, cb) {
+    request({ url: url,
+              headers: {'User-Agent': 'request'}}, cb)
+              .auth('Issuefix', 'YcQbNMCkYMhcZUtfLm5Qjj8o8zjg67');
+}
+
+function addRepo(repo) {
+    try {
+        db.run(`INSERT INTO projects (project_name) VALUES($1);`, [repo.full_name], (err, row) => {
+            console.log('Added Repo: ' + repo.full_name)
+            return
+        });
+    } catch(err) {
+        console.log(err)
+        return
+    }
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomProject(rows, cb) {
+    let rand = getRandomInt(0, rows.length)
+    let project = rows[rand]
+
+    console.log('Getting Issues for Project: ' + project.project_name)
+    requestWithUserAgent(apiUrl + `/repos/${project.project_name}/issues`, (error, response, body) => {
+        let issues = JSON.parse(body)
+        if (issues.length < 1) {
+            rows.splice()
+            getRandomProject(rows, cb);
+        } else {
+            let issue = JSON.stringify(issues[getRandomInt(0, issues.length)])
+            cb(issue)
+            return
+        }  
+    });
+}
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/issue', (req, res) => {
+    db.all('SELECT * FROM projects', (err, rows) => {
+        getRandomProject(rows, (issue) => {
+            res.send(issue)
+        });
+    });
+});
+
+app.get('/add/user/:user', (req, res) => {
+    requestWithUserAgent(apiUrl + `/users/${req.params.user}/repos`, (error, response, body) => {
+        if (response.statusCode === 404) {
+            res.send('User not Found: ' + req.params.user)
+        } else {
+            let repos = JSON.parse(body)
+            repos.forEach(addRepo)
+
+            res.send('User Added!')
+            console.log('Added User: ' + req.params.user)
+            return
+        }
+    });
+});
+
+app.get('/add/repo/:user/:repo', (req, res) => {
+    requestWithUserAgent(apiUrl + `/repos/${req.params.user}/${req.params.repo}`, (error, response, body) => {
+         if (response.statusCode === 404) {
+            res.send('Repo not Found: ' + `${req.params.user}/${req.params.repo}`)
+         } else {
+            addRepo(JSON.parse(body))
+            res.send('Repo Added!')
+            return
+         }
+    });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('# Issuefix Server Started')
+});
